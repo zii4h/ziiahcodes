fe6de63e33135526d9eadd5cc2fae3a4570d1749
Sophia <sophiakeziahgpineda@gmail.com>
2026-04-24 00:44:51 +0800

    add: image photocards

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import fs from 'fs'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'copy-cname',
+      closeBundle() {
+        fs.writeFileSync(resolve(__dirname, 'dist/CNAME'), 'ziiah.net')
+      }
+    }
+  ],
 })
