15a6f73a090148a5814b55f3bd0c3f8f9f2ca1e9
Sophia <sophiakeziahgpineda@gmail.com>
2026-05-12 01:39:03 +0800

    chore: skills text

--- src/components/HomeClient.jsx
+++ src/components/HomeClient.jsx
@@ -15,8 +15,8 @@ export default function HomeClient() {
   const certsGridRef = useRef(null);
 
   const skills = {
-    "Libraries & Frameworks": ["ReactJS", "NodeJS", "ShadCN UI", "Astro", "Vanilla JS"],
-    "Tools & Platforms": ["GitHub", "Git", "Vite", "Vercel", "Docker", "Jira-Notion", "Figma", "Framer-Canva", "Affinity", "MS Office Tools"],
+    "Libraries & Frameworks": ["ReactJS", "NextJS", "NodeJS", "ShadCN UI", "Astro", "Vanilla JS"],
+    "Tools & Platforms": ["GitHub", "Vite", "Vercel", "Docker", "Jira-Notion", "Figma", "Framer-Canva", "Affinity", "MS Office Tools"],
     "Programming Languages": ["JavaScript", "TypeScript", "Python", "Ruby", "HTML-CSS-JS"],
     "Database & Workbench": ["SQL", "MySQL", "Oracle", "Supabase / PostgreSQL", "XAMPP / phpMyAdmin", "Snowflake", "DataDog"],
     "Artificial Intelligence": ["Guardrails", "Claude", "OpenAI", "Ollama", "GPT-4"],
